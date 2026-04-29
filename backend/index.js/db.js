8a2522bec8b369ea04cc03c3aa6f3fd99d63bc33
JeevanSai04 <jeevan810sai@gmail.com>
2026-04-29 16:14:06 +0530

    fixed db connection using env

--- backend/index.js/db.js
+++ backend/index.js/db.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:jeevans2522j_@db.tjlxmsrlhchbicnzzdbb.supabase.co:5432/postgres",
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
